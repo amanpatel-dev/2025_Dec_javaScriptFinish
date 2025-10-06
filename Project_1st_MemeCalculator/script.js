b310690bf35d411a2848b16f30a2445929fb16e2
amanchaudhary01 <aman.patel7474@gmail.com>
2025-10-06 21:23:49 +0530

    Minor change in onclick function

--- Project_1st_MemeCalculator/script.js
+++ Project_1st_MemeCalculator/script.js
@@ -1,35 +1,26 @@
 let numStr = "";
 let calcu = [];
+let storeString="";
 
-function storeVal(el) {
-  btnVal = el.getAttribute("value");
-
-  //Array to 
-  const operators = ["+", "-", "*", "/",'=', '(', ')'];
-  const lastChar = numStr[numStr.length - 1];
-
-  document.getElementById("myImage").src = "./img/61.thumb128.png";
-
-  if (operators.includes(lastChar) && operators.includes(btnVal)) {
-    console.log("hi");
-    document.getElementById("myImage").src = "./img/pakistaniguy.webp";
-    return; 
-    }
-    
-    numStr += btnVal;
-
-   if (!isNaN(btnVal) && btnVal !== '') {
-        // If it's a number, convert the string to a number before pushing
-        calcu.push(Number(btnVal));
-    } else {
-        // If it's an operator, push it as a string
-        calcu.push(btnVal);
-    }
- 
-  
-  console.log(calcu);
-  document.getElementById("num-input").textContent = numStr;
-}
+// function storeVal(el) {
+//   btnVal = el.getAttribute("value");
+//   const operators = ["+", "-", "*", "/",'=', '(', ')'];
+//   const lastChar = numStr[numStr.length - 1];
+//   // this is to check not two operator is entered
+//   if (operators.includes(lastChar) && operators.includes(btnVal)) {
+//     console.log("Can't enter operator twice");
+//     return; 
+//     }
+//     numStr += btnVal;
+//    if (!isNaN(btnVal) && btnVal !== '') {
+//         // If it's a number, convert the string to a number before pushing
+//         calcu.push(Number(btnVal));
+//     } else {
+//         // If it's an operator, push it as a string
+//         calcu.push(btnVal);
+//     }
+//   document.getElementById("num-input").textContent = numStr;
+// }
 
 function resVal(el) {
   // result = eval(numStr);
@@ -38,20 +29,103 @@ function resVal(el) {
   // [2, '*', 4, '+', '(', 3, '*', 5, '/', 3, '+', 4, '-', 2, '*', '(', 8, '-', 2, ')', ')', '+', 3, '-', 2, '*', 4]
   // numStr = "";
   // console.log(calcu);
-
+  calcu.push(Number(numStr));
+  console.log("This is the LAsT array:"+calcu);
   const rpnExpression =shuntingYard(calcu);
-  console.log(rpnExpression);
+  console.log("This is the LAsT  Rpn Expression:"+rpnExpression);
   const finalResult=calculateRpn(rpnExpression)
-  console.log(finalResult);
+  console.log("This is the LAsT  final Result:"+finalResult);
   document.getElementById("num-result").textContent = "=" + finalResult;
+  numStr="";
+  calcu=[];
 
 }
 
 function clearScreen(el){
     document.getElementById("num-result").textContent = "";
     document.getElementById("num-input").textContent = "";
-      document.getElementById("myImage").src = "./img/aayein.webp";
+    numStr='';
+    storeString='';
+    calcu=[];
+
+    console.log("Clear Funtion");
+    console.log("Numstr :"+ numStr);
+    console.log(" calcuArray :" + calcu);
+      // document.getElementById("myImage").src = "./img/aayein.webp";
 }
+
+function onNumberClick(el){
+  btnVal = el.getAttribute("value");
+  numStr += btnVal;
+  storeString +=btnVal;
+  document.getElementById("num-input").textContent = storeString;
+  console.log(numStr);
+
+}
+function onOperatorClick(el){
+  
+  btnVal = el.getAttribute("value");
+  if(numStr===''){
+    alert('Please enter an operation or dont press same operatoin twice');
+    return;
+  }
+  const operators = ["+", "-", "*", "/",'=', '(', ')'];
+  const lastChar = numStr[numStr.length - 1];
+  if (operators.includes(lastChar) && operators.includes(btnVal)) {
+    console.log("Can't enter operator twice");
+    return; 
+    }
+    else{
+      console.log("this is inside op function" + numStr);
+      calcu.push(Number(numStr));
+      numStr+=btnVal;
+      calcu.push(btnVal); 
+      storeString+=btnVal;
+      document.getElementById("num-input").textContent = storeString;
+
+      numStr="";
+      console.log(calcu);
+    }
+
+}
+
+/* chatgpt help
+
+let currentString= "";
+let expressionArray= [];
+
+function onNumberClick(num){
+  currentString += num;
+  updateDisplay(currentString);
+}
+
+function onOperatorClick(op){
+  if(currentString !== ""){
+    expressionArray.push(currentString);
+    currentString="";
+  }
+  expressionArray.push(op);
+  updateDisplay(expressionArray.join(" "));
+}
+
+function onEqualClick(){
+  if(currentString !== ""){
+    expressionArray.push(currentString);
+  }
+
+  let expression = expressionArray.join(" ");
+  let result= shuntingYard(expression);
+  updateDisplay(result);
+  currentString=result.toString();
+  expressionArray=[];
+}
+
+function updateDisplay(content){
+  document.getElementById("num-result").textContent = "=" + content;
+
+}
+*/
+
 
 
 // const rpnExpression =shuntingYard(calcu);
@@ -105,59 +179,7 @@ while(operatorStack.length>0){
 return outputQueue;
 }
 
-// function shuntingYard(infixArray) {
-//   const outputQueue = [];
-//   const operatorStack = [];
-  
-//   // Define operator precedence
-//   const precedence = {
-//     '+': 1,
-//     '-': 1,
-//     '*': 2,
-//     '/': 2
-//   };
-  
-//   for (const token of infixArray) {
-//     if (typeof token === 'number') {
-//       // If the token is a number, add it to the output queue.
-//       outputQueue.push(token);
-//     } else if (token in precedence) {
-//       // If the token is an operator, check the stack.
-//       const currentPrecedence = precedence[token];
-//       while (
-//         operatorStack.length > 0 &&
-//         operatorStack[operatorStack.length - 1] !== '(' &&
-//         precedence[operatorStack[operatorStack.length - 1]] >= currentPrecedence
-//       ) {
-//         // Pop operators from the stack to the output as long as they have higher or equal precedence.
-//         outputQueue.push(operatorStack.pop());
-//       }
-//       operatorStack.push(token);
-//     } else if (token === '(') {
-//       // If a left parenthesis, push it onto the stack.
-//       operatorStack.push(token);
-//     } else if (token === ')') {
-//       // If a right parenthesis, pop operators from the stack to the output
-//       // until a left parenthesis is found.
-//       while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] !== '(') {
-//         outputQueue.push(operatorStack.pop());
-//       }
-//       // Discard the left parenthesis.
-//       if (operatorStack[operatorStack.length - 1] === '(') {
-//         operatorStack.pop();
-//       }
-//     }
-//   }
 
-//   // After processing all tokens, pop any remaining operators from the stack to the output.
-//   while (operatorStack.length > 0) {
-//     outputQueue.push(operatorStack.pop());
-//   }
-  
-//   return outputQueue;
-// }
-
-// const finalResult=calculateRpn(rpnExpression)
 
 function calculateRpn(rpnArray){
   const stack=[];
